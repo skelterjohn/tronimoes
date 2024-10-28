7d91aefa3df28743795406a13dcd4e900c2dfd46
John Asmuth <jasmuth@gmail.com>
2024-10-28 13:08:53 -0400

    improved borders

--- tronapp/src/app/board/Tile.js
+++ tronapp/src/app/board/Tile.js
@@ -34,11 +34,12 @@ function Tile({pipsa, pipsb, orientation}) {
 		return (
 			<div className={`h-full w-full ${rotate}`}>
 				<div className="h-[200%] w-[100%] p-1">
-					<div className="w-full h-full bg-white border-black rounded-lg border-2">
+					<div className="w-full h-full bg-white border-black rounded-lg border-4">
 						<table className="w-full h-full table-fixed">
 							<tbody>
 								<tr><td>
 									<TileHalf pips={pipsa} orientation={"down"}/>
+									<div className="absolute bottom-[-2px] left-[15%] w-[70%] h-[4px] bg-gray-300"></div>
 								</td></tr>
 								<tr><td>
 									<TileHalf pips={pipsb} orientation={"up"}/>
