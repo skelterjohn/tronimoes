ce977874054715ef0dc5a553008abd1b004d696d
John Asmuth <jasmuth@gmail.com>
2024-10-29 17:31:09 -0400

    added a landing page

--- tronapp/src/app/page.js
+++ tronapp/src/app/page.js
@@ -1,12 +1,48 @@
+"use client";
+
+import Image from 'next/image';
+import { Input, Button } from 'antd';
+
 export default function Home() {
-  return (
-    <div className="items-center justify-items-center min-h-screen h-screen">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full  max-w-screen-md">
-        let's play a game
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        JT
-      </footer>
-    </div>
-  );
+	return (
+		<div className="relative min-h-screen flex items-center justify-center">
+			<Image 
+			src="/trondude.png"
+			alt="Background"
+			fill
+			className="object-cover z-0"
+			priority
+			/>
+
+			<div className="relative z-10 flex flex-col gap-4 text-white">
+				<Input
+					placeholder="name"
+					size="large"
+					className="text-lg"
+				/>
+				<div className="flex gap-2">
+					<Input
+						placeholder="code"
+						size="large"
+						className="text-lg"
+					/>
+					<Button
+						type="primary"
+						size="large"
+					>
+						join
+					</Button>
+				</div>
+				<div className="flex w-full gap-2">
+					<Button
+						className="w-full"
+						type="primary"
+						size="large"
+					>
+						random
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
