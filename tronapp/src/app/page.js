d146c20581d278a2c21a71f50472367c15b7ac60
John Asmuth <jasmuth@gmail.com>
2024-10-29 14:30:14 -0400

    simplify main page

--- tronapp/src/app/page.js
+++ tronapp/src/app/page.js
@@ -3,9 +3,8 @@ import Game from "./components/game/Game";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-items-center min-h-screen h-screen">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full  max-w-screen-md">
-        Tronimes
         <Game/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
