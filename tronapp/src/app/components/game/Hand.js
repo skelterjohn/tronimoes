91bc6277389b902996feec87d658cf900f80a83e
John Asmuth <jasmuth@gmail.com>
2024-11-02 16:06:37 -0400

    add a draw button

--- tronapp/src/app/components/game/Hand.js
+++ tronapp/src/app/components/game/Hand.js
@@ -1,25 +1,39 @@
 import Tile from '../board/Tile';
+import { Button } from "antd";
+import { useEffect } from "react";
 
-function Hand({ name, color="white", hidden=false, tiles=[], dead=false, selectedTile, setSelectedTile, playerTurn}) {
+function Hand({ name, color="white", hidden=false, tiles=[], dead=false, selectedTile, setSelectedTile, playerTurn, drawTile}) {
 	function tileClicked(tile) {
-		if (hidden) return;
+		if (hidden) {
+			return;
+		}
 		setSelectedTile(tile);
 	}
 
 	return <div className="flex flex-col items-center gap-2">
 		<div className="text-center font-bold">{name}</div>
-		<div className="flex items-center justify-center">{tiles.map((t, i) => (
-			<div key={i} className="max-w-[10%] " onClick={()=>tileClicked(t)}>
-				<Tile
-					color={color}
-					pipsa={t.a}
-					pipsb={t.b}
-					back={hidden}
-					dead={dead}
-					selected={playerTurn && selectedTile!==undefined && t.a===selectedTile.a && t.b===selectedTile.b}
-				/>
-			</div>
-		))}</div>
+		<div className="flex items-center justify-center">
+			{tiles.map((t, i) => (
+				<div key={i} className="max-w-[10%] " onClick={()=>tileClicked(t)}>
+					<Tile
+						color={color}
+						pipsa={t.a}
+						pipsb={t.b}
+						back={hidden}
+						dead={dead}
+						selected={playerTurn && selectedTile!==undefined && t.a===selectedTile.a && t.b===selectedTile.b}
+					/>
+				</div>
+			))}
+			{!hidden && <span>
+				<Button
+					disabled={!playerTurn}
+					onClick={drawTile}
+				>
+					Draw
+				</Button>
+			</span>}
+		</div>
 	</div>;
 }
 
