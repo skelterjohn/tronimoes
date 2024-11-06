418f3caa849d0fa3fca1706788d4489c1ff893de
John Asmuth <jasmuth@gmail.com>
2024-11-06 15:37:20 -0500

    center the board

--- tronapp/src/app/gameboard/[code]/page.js
+++ tronapp/src/app/gameboard/[code]/page.js
@@ -2,8 +2,8 @@ import Game from "../../components/game/Game";
 
 export default function Gameboard({params}) {
   return (
-    <div className="h-screen flex">
-      <main className="flex-1 flex flex-col gap-2 items-center sm:items-start w-full max-h-screen">
+    <div className="h-screen flex justify-center">
+      <main className="flex-1 flex flex-col gap-2 items-center justify-center w-full max-h-screen">
         <Game code={params.code} />
       </main>
     </div>
