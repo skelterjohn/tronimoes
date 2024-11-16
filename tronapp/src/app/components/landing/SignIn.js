dca946bdf0bffdcafcabbc80fef61d90227a512a
John Asmuth <jasmuth@gmail.com>
2024-11-16 13:37:21 -0500

    added google sign-on

--- tronapp/src/app/components/landing/SignIn.js
+++ tronapp/src/app/components/landing/SignIn.js
@@ -1,5 +1,5 @@
 import { Modal } from "antd";
-import { signInAnonymously } from "firebase/auth";
+import { signInAnonymously, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Button } from "antd";
 import { auth } from "@/config";
 
@@ -16,11 +16,25 @@ export default function SignIn({userInfo, setUserInfo, isOpen, onClose}) {
 		}
 	};
 
+	const signInWithGoogle = async () => {
+		try {
+			const provider = new GoogleAuthProvider();
+			const result = await signInWithPopup(auth, provider);
+			setUserInfo(result.user);
+			onClose();
+		} catch (error) {
+			console.error("Error signing in with Google:", error);
+		}
+	};
+
 	return (
 		<Modal open={isOpen} title="sign in to tronimoes" onCancel={onClose} footer={null} centered width={800}>
 			<div className="flex flex-col items-center gap-4 p-4">
+				<Button onClick={signInWithGoogle} size="large">
+					Sign in with Google
+				</Button>
 				<Button onClick={signInAsGuest} size="large">
-					Browser Session
+					Anonymous
 				</Button>
 			</div>
 		</Modal>
