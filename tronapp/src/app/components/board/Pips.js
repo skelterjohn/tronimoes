88147c40edbb4fa8e63d5e4928319888444e1085
John Asmuth <jasmuth@gmail.com>
2024-11-17 21:53:37 -0500

    illustrate free line start

--- tronapp/src/app/components/board/Pips.js
+++ tronapp/src/app/components/board/Pips.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 
 export default function Pips({pips}) {
 	if (pips === -1) {
-		return <div></div>
+		return (
+			<div className="w-full h-full flex justify-center items-center flex-col border border-black rounded-lg bg-white">
+				<div>FREE LINE</div>
+				<div>SPACER</div>
+				<div>START</div>
+			</div>
+		);
 	}
 	return (
 		<div className="w-full flex justify-center items-center">
