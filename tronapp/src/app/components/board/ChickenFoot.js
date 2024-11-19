21bb7bbd2dc026610c0b41e76a58f0335408e43d
John Asmuth <jasmuth@gmail.com>
2024-11-19 17:05:42 -0500

    chickenfoot color adjustments

--- tronapp/src/app/components/board/ChickenFoot.js
+++ tronapp/src/app/components/board/ChickenFoot.js
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 export default function ChickenFoot({ url, color }) {
 	const colorMap = {
-        red: "bg-red-500",
-        blue: "bg-blue-500",
-        green: "bg-green-500",
-        yellow: "bg-yellow-500",
-        orange: "bg-orange-500",
-        yellow: "bg-yellow-500",
-        purple: "bg-purple-500",
+        red: "bg-red-700",
+        blue: "bg-blue-700",
+        green: "bg-green-700",
+        yellow: "bg-yellow-700",
+        orange: "bg-orange-700",
+        yellow: "bg-yellow-700",
+        purple: "bg-purple-700",
         transparent: "bg-transparent"
     };
 
 	return (
         <div className="w-full h-full flex items-center justify-center">
-            <div className={`w-3/4 h-3/4 rounded-full ${colorMap[color]} opacity-50 relative`}>
+            <div className={`w-3/4 h-3/4 rounded-lg ${colorMap[color]} border border-black relative`}>
             </div>
             {url && <div className="w-3/4 h-3/4 absolute">
                 <img src={url} alt="Chicken Foot" className="w-full h-full object-contain" />
