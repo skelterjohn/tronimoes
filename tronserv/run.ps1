e52fb712e6917fd14c1ccb98afecb4d52e4c2be8
John Asmuth <jasmuth@gmail.com>
2024-11-26 12:40:34 -0500

    install first, then build

--- tronserv/run.ps1
+++ tronserv/run.ps1
@@ -1,6 +1,6 @@
 echo "Building tronserv..."
-go build -o tronserv.exe .
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 go install .
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+go build -o tronserv.exe .
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 .\tronserv.exe $args
