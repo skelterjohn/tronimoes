8b06babc405c5009c3ad24366b043fb213b488a6
John Asmuth <jasmuth@gmail.com>
2024-11-26 12:38:36 -0500

    try to benefit from pre-built objects

--- tronserv/run.ps1
+++ tronserv/run.ps1
@@ -1,4 +1,6 @@
 echo "Building tronserv..."
 go build -o tronserv.exe .
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+go install .
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 .\tronserv.exe $args
