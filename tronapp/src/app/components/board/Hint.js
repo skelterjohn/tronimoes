d6aacd733b964efa6989f89e11c6a40e9e5a196e
John Asmuth <jasmuth@gmail.com>
2024-11-28 23:21:51 -0500

    clearer hints

--- tronapp/src/app/components/board/Hint.js
+++ tronapp/src/app/components/board/Hint.js
@@ -1,7 +1,7 @@
 export default function Hint() {
 	return (
         <div className="w-full h-full flex items-center justify-center">
-            <div className={`w-[90%] h-[90%] border-black border opacity-50`}></div>
+            <div className={`w-[90%] h-[90%] border-white border-4 border`}></div>
         </div>
     )
 }
