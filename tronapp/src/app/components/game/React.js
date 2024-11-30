d477efa4bca2b9b5da225339fadd5bb9b104c284
John Asmuth <jasmuth@gmail.com>
2024-11-30 13:05:35 -0500

    timeout the react

--- tronapp/src/app/components/game/React.js
+++ tronapp/src/app/components/game/React.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+
 export default function React({
 	show, setShow, url
 }) {
 	if (!show) {
 		return null;
 	}
+
+	useEffect(() => {
+		if (!show) {
+			return;
+		}
+		setTimeout(() => {
+			setShow(false);
+		}, 10000);
+	}, [show]);
+
 	return <div
 		onClick={() => setShow(false)}
 		className="absolute z-50 pointer-events-none"
