07e94c3f12b9391cabf69f4aa3cb93baa86a027a
John Asmuth <jasmuth@gmail.com>
2026-01-31 17:34:40 -0500

    more flexible client helper

--- tronserv/client/client.go
+++ tronserv/client/client.go
@@ -1,10 +1,11 @@
 package client
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
 
 	"github.com/skelterjohn/tronimoes/tronserv/game"
@@ -21,30 +22,40 @@ func (c *TronimoesClient) WriteHeaders(req *http.Request) {
 	req.Header.Set("X-Player-Name", c.Name)
 }
 
-func (c *TronimoesClient) Get(ctx context.Context, path string) (*http.Response, error) {
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/%s", c.TronservAddr, path), http.NoBody)
+func (c *TronimoesClient) Do(ctx context.Context, method, path string, vin, vout any) error {
+	var body io.Reader
+	if vin == nil {
+		body = http.NoBody
+	} else {
+		b := &bytes.Buffer{}
+		if err := json.NewEncoder(b).Encode(vin); err != nil {
+			return fmt.Errorf("could not encode request body: %v", err)
+		}
+	}
+	req, err := http.NewRequestWithContext(ctx, method, fmt.Sprintf("%s/%s", c.TronservAddr, path), body)
 	if err != nil {
-		return nil, fmt.Errorf("could not create request: %s", err)
+		return fmt.Errorf("could not create request: %s", err)
 	}
 
-	return c.Client.Do(req)
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("could not do request: %s", err)
+	}
+
+	if vout != nil {
+		if err := json.NewDecoder(resp.Body).Decode(vout); err != nil {
+			return fmt.Errorf("could not decode response: %s", err)
+		}
+	}
+
+	return nil
 }
 
 func (c *TronimoesClient) GetPlayer(ctx context.Context, name string) (*game.PlayerInfo, error) {
-	resp, err := c.Get(ctx, "players/jt")
-	if err != nil {
-		return nil, fmt.Errorf("could not do request: %s", err)
-	}
-
 	var pi game.PlayerInfo
-
-	if err := json.NewDecoder(resp.Body).Decode(&pi); err != nil {
-		return nil, fmt.Errorf("could not decode player: %v", err)
+	if err := c.Do(ctx, "GET", "players/jt", nil, &pi); err != nil {
+		return nil, err
 	}
-	if err := resp.Body.Close(); err != nil {
-		log.Printf("Error closing player response: %v", err)
-	}
-
 	fmt.Printf("%+v\n", pi)
 
 	return nil, nil
