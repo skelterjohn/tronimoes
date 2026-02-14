c56009ff3071fbcb6f83a27a7ac04e5b21b8e2f9
John Asmuth <jasmuth@gmail.com>
2026-02-14 15:52:37 -0500

    refactor to allow a nicer toc code

--- tronapp/src/app/rules/page.js
+++ tronapp/src/app/rules/page.js
@@ -22,12 +22,145 @@ function Section({ title, children }) {
 	);
 }
 
-const SECTION_TITLES = [
-	"Leaders and lines",
-	"A game of murder",
-	"A dead line",
-	"Drawing and passing",
-	"The dreaded chicken-foot",
+const SECTIONS = [
+	{
+		title: "Leaders and lines",
+		content: (
+			<>
+				<p>
+					In tronimoes, players take turns laying tiles on the board. They start
+					from the central tile, known as the "round leader". Lines are built by
+					matching pips from tile to tile.
+				</p>
+				<p>
+					Here, there are two players: red and blue. They both have lines beginning
+					from the 3:3 round leader. All players must begin their line from the
+					round leader.
+				</p>
+				<p>
+					The goal is to win the round, which can be accomplished in a two ways: 
+					being first to run out of tiles, or being the last player standing.
+				</p>
+				<RulesBoard
+					height={7}
+					tiles={{
+						"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
+						"2,2": { a: 3, b: 5, orientation: "up", color: "red", dead: false },
+						"4,3": { a: 3, b: 7, orientation: "up", color: "blue", dead: false },
+						"2,0": { a:5, b:1, orientation: "left", color: "red", dead: false },
+						"4,1": { a: 7, b:0, orientation: "up", color: "blue", dead: false },
+					}}
+					roundLeader={{ pips_a: 3, pips_b: 3 }}
+					lineHeads={[
+						{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 2, y: 3 } },
+					]}
+					activePlayer={{ color: "red" }}
+				/>
+			</>
+		),
+	},
+	{
+		title: "A game of murder",
+		content: (
+			<>
+				<p>
+					If a player lays a tile that makes it impossible for another player to
+					continue their line, that player is "killed", and their line is "dead".
+				</p>
+				<p>
+					Their tiles remain on the board, blocking others.
+				</p>
+				<p>What move can blue make to win this round?</p>
+				<RulesBoard
+					height={7}
+					tiles={{
+						"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
+						"2,2": { a: 3, b: 5, orientation: "up", color: "red", dead: false },
+						"3,2": { a: 3, b: 7, orientation: "up", color: "blue", dead: false },
+						"2,0": { a: 5, b:3, orientation: "right", color: "red", dead: false },
+					}}
+					roundLeader={{ pips_a: 3, pips_b: 3 }}
+					lineHeads={[
+						{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 2, y: 3 } },
+					]}
+					activePlayer={{ color: "blue" }}
+				/>
+			</>
+		),
+	},
+	{
+		title: "A dead line",
+		content: (
+			<>
+				<p>
+					That's right (probably): blue can place their next tile blocking
+					red from continuing.
+				</p>
+				<p>
+					Unfortunately for red, the edges of the board block them on one side,
+					and blue's tiles block the other. There is no way to play more tiles
+					on that line, so it's dead.
+				</p>
+				<RulesBoard
+					height={7}
+					tiles={{
+						"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
+						"2,2": { a: 3, b: 5, orientation: "up", color: "red", dead: true },
+						"3,2": { a: 3, b: 7, orientation: "up", color: "blue", dead: false },
+						"2,0": { a: 5, b:3, orientation: "right", color: "red", dead: true },
+						"4,1": { a: 7, b:0, orientation: "up", color: "blue", dead: false },
+					}}
+					roundLeader={{ pips_a: 3, pips_b: 3 }}
+					lineHeads={[
+						{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 2, y: 3 } },
+					]}
+					activePlayer={false}
+				/>
+			</>
+		),
+	},
+	{
+		title: "Drawing and passing",
+		content: (
+			<>
+				<p>
+					On your turn, you may opt to draw a tile, if you haven't already.
+				</p>
+				<p>
+					Once you've drawn a tile, you may opt to pass your turn.
+				</p>
+				<p>
+					You can do this draw/pass maneuver whether or not you have a tile
+					that could have been laid.
+				</p>
+			</>
+		),
+	},
+	{
+		title: "The dreaded chicken-foot",
+		content: (
+			<>
+				<p>
+					If you pass, and you weren't chicken-footed already, you become
+					chicken-footed.
+				</p>
+				<p>
+					When you are chicken-footed, other players may lay tiles on your
+					line, and you may only play on your own line.
+				</p>
+				<p>
+					Other players are likely to do bad things to you. If they have your
+					line box itself in, you're dead and they get the credit (it's the
+					player who laid the tile that made it so you couldn't continue that
+					gets points for the kill).
+				</p>
+				<p>
+					Once you are finally able to play a tile on your own line again, you
+					are no longer chicken-footed.
+				</p>
+			</>
+		),
+	},
 ];
 
 export default function RulesPage() {
@@ -66,132 +199,23 @@ export default function RulesPage() {
 						className="w-52 flex-shrink-0 overflow-y-auto border-r border-slate-600 bg-slate-800 py-4"
 					>
 						<nav className="px-4 space-y-1">
-							{SECTION_TITLES.map((t) => (
+							{SECTIONS.map((section) => (
 								<a
-									key={t}
-									href={`#${slugify(t)}`}
+									key={section.title}
+									href={`#${slugify(section.title)}`}
 									className="block py-1.5 text-sm text-slate-300 hover:text-slate-100 focus:text-slate-100 focus:outline-none underline-offset-2 hover:underline"
 								>
-									{t}
+									{section.title}
 								</a>
 							))}
 						</nav>
 					</aside>
 					<div className="flex-1 overflow-y-auto">
-				<Section title="Leaders and lines">
-					<p>
-						In tronimoes, players take turns laying tiles on the board. They start
-						from the central tile, known as the "round leader". Lines are built by
-						matching pips from tile to tile.
-					</p>
-					<p>
-						Here, there are two players: red and blue. They both have lines beginning
-						from the 3:3 round leader. All players must begin their line from the
-						round leader.
-					</p>
-					<p>
-						The goal is to win the round, which can be accomplished in a two ways: 
-						being first to run out of tiles, or being the last player standing.
-					</p>
-					<RulesBoard
-						height={7}
-						tiles={{
-							"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
-							"2,2": { a: 3, b: 5, orientation: "up", color: "red", dead: false },
-							"4,3": { a: 3, b: 7, orientation: "up", color: "blue", dead: false },
-							"2,0": { a:5, b:1, orientation: "left", color: "red", dead: false },
-							"4,1": { a: 7, b:0, orientation: "up", color: "blue", dead: false },
-						}}
-						roundLeader={{ pips_a: 3, pips_b: 3 }}
-						lineHeads={[
-							{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 2, y: 3 } },
-						]}
-						activePlayer={{ color: "red" }}
-					/>
-				</Section>
-				<Section title="A game of murder">
-					<p>
-						If a player lays a tile that makes it impossible for another player to
-						continue their line, that player is "killed", and their line is "dead".
-					</p>
-					<p>
-						Their tiles remain on the board, blocking others.
-					</p>
-					<p>What move can blue make to win this round?</p>
-
-					<RulesBoard
-						height={7}
-						tiles={{
-							"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
-							"2,2": { a: 3, b: 5, orientation: "up", color: "red", dead: false },
-							"3,2": { a: 3, b: 7, orientation: "up", color: "blue", dead: false },
-							"2,0": { a: 5, b:3, orientation: "right", color: "red", dead: false },
-						}}
-						roundLeader={{ pips_a: 3, pips_b: 3 }}
-						lineHeads={[
-							{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 2, y: 3 } },
-						]}
-						activePlayer={{ color: "blue" }}
-					/>
-				</Section>
-				<Section title="A dead line">
-					<p>
-						That's right (probably): blue can place their next tile blocking
-						red from continuing.
-					</p>
-					<p>
-						Unfortunately for red, the edges of the board block them on one side,
-						and blue's tiles block the other. There is no way to play more tiles
-						on that line, so it's dead.
-					</p>
-					<RulesBoard
-						height={7}
-						tiles={{
-							"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
-							"2,2": { a: 3, b: 5, orientation: "up", color: "red", dead: true },
-							"3,2": { a: 3, b: 7, orientation: "up", color: "blue", dead: false },
-							"2,0": { a: 5, b:3, orientation: "right", color: "red", dead: true },
-							"4,1": { a: 7, b:0, orientation: "up", color: "blue", dead: false },
-						}}
-						roundLeader={{ pips_a: 3, pips_b: 3 }}
-						lineHeads={[
-							{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 2, y: 3 } },
-						]}
-						activePlayer={false}
-					/>
-				</Section>
-				<Section title="Drawing and passing">
-					<p>
-						On your turn, you may opt to draw a tile, if you haven't already.
-					</p>
-					<p>
-						Once you've drawn a tile, you may opt to pass your turn.
-					</p>
-					<p>
-						You can do this draw/pass maneuver whether or not you have a tile
-						that could have been laid.
-					</p>
-				</Section>
-				<Section title="The dreaded chicken-foot">
-					<p>
-						If you pass, and you weren't chicken-footed already, you become
-						chicken-footed.
-					</p>
-					<p>
-						When you are chicken-footed, other players may lay tiles on your
-						line, and you may only play on your own line.
-					</p>
-					<p>
-						Other players are likely to do bad things to you. If they have your
-						line box itself in, you're dead and they get the credit (it's the
-						player who laid the tile that made it so you couldn't continue that
-						gets points for the kill).
-					</p>
-					<p>
-						Once you are finally able to play a tile on your own line again, you
-						are no longer chicken-footed.
-					</p>
-				</Section>
+						{SECTIONS.map((section) => (
+							<Section key={section.title} title={section.title}>
+								{section.content}
+							</Section>
+						))}
 					</div>
 				</div>
 				<Settings
