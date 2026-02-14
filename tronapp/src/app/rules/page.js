083e877b712700818dff17ca80c15b28090da107
John Asmuth <jasmuth@gmail.com>
2026-02-14 14:27:29 -0500

    a bit of sanity to the example board

--- tronapp/src/app/rules/page.js
+++ tronapp/src/app/rules/page.js
@@ -6,25 +6,17 @@ import { TipProvider } from "@/app/components/tutorial/InnerTip";
 import { GameContext } from "@/app/components/GameState";
 import { useGameState } from "@/app/components/GameState";
 
-const RULES_BOARD_WIDTH = 6;
 const RULES_BOARD_HEIGHT = 7;
+const RULES_BOARD_WIDTH = RULES_BOARD_HEIGHT - 1;
 
 const rulesTiles = {
-	"3,2": { a: 3, b: 3, orientation: "down", color: "white", dead: false },
-	"4,2": { a: 3, b: 1, orientation: "right", color: "white", dead: false },
-	"5,2": { a: 1, b: 4, orientation: "right", color: "white", dead: false },
-	"2,2": { a: 3, b: 6, orientation: "left", color: "white", dead: false },
-	"1,2": { a: 6, b: 0, orientation: "left", color: "white", dead: false },
-	"3,3": { a: 3, b: 2, orientation: "down", color: "white", dead: false },
-	"3,4": { a: 2, b: 5, orientation: "down", color: "white", dead: false },
+	"2,3": { a: 3, b: 3, orientation: "right", color: "white", dead: false },
 };
 
 const rulesRoundLeader = { pips_a: 3, pips_b: 3 };
 
 const rulesLineHeads = [
-	{ tile: { pips_a: 1, pips_b: 4 }, coord: { x: 5, y: 2 } },
-	{ tile: { pips_a: 2, pips_b: 5 }, coord: { x: 3, y: 4 } },
-	{ tile: { pips_a: 6, pips_b: 0 }, coord: { x: 1, y: 2 } },
+	{ tile: { pips_a: 3, pips_b: 3 }, coord: { x: 3, y: 2 } },
 ];
 
 const noop = () => {};
