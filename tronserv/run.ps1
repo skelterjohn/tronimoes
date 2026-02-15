7e2233936d3e9c7adf3099b79ce1082c4de756df
John Asmuth <jasmuth@gmail.com>
2026-02-15 16:16:19 -0500

    no CORS when running local dev

--- tronserv/run.ps1
+++ tronserv/run.ps1
@@ -3,4 +3,4 @@ go install .
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 go build -o tronserv.exe .
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-.\tronserv.exe $args
+.\tronserv.exe --no-cors $args
