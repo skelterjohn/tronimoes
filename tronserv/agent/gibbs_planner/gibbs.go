9006e3c57470f384ce92bc5cd917b8d2d3063380
John Asmuth <jasmuth@gmail.com>
2026-02-24 22:38:30 -0500

    refactor guesswork out of the way

--- tronserv/agent/gibbs_planner/gibbs.go
+++ tronserv/agent/gibbs_planner/gibbs.go
@@ -10,7 +10,11 @@ import (
 )
 
 type HandState struct {
-	tiles []game.Tile
+	tiles         []game.Tile
+	justDrew      bool
+	justPassed    bool
+	justLaid      game.Tile
+	opportunities []game.Tile
 }
 
 type GibbsPlanner struct {
@@ -50,97 +54,9 @@ func (gp *GibbsPlanner) RemoveTileFromHand(ctx context.Context, whichPlayer int,
 
 func (gp *GibbsPlanner) Update(ctx context.Context, g *game.Game) {
 	if gp.lastGame == nil {
-		for i, p := range g.Players {
-			if p.Name == gp.Name {
-				gp.myPlayerIndex = i
-				break
-			}
-		}
-		// A round just started, let's halucinate a bag and deal it out.
-		gp.bag = nil
-		for a := 0; a <= g.MaxPips; a++ {
-			for b := a; b <= g.MaxPips; b++ {
-				gp.bag = append(gp.bag, game.Tile{
-					PipsA: a,
-					PipsB: b,
-				})
-			}
-		}
-		rand.Shuffle(len(gp.bag), func(i, j int) {
-			gp.bag[i], gp.bag[j] = gp.bag[j], gp.bag[i]
-		})
-		for i, p := range g.Players {
-			if i == gp.myPlayerIndex {
-				// we know our own hand.
-				log.Printf("our own hand: %v", p.Hand)
-				gp.hands = append(gp.hands, &HandState{})
-				for _, pts := range p.Hand {
-					gp.RemoveTileFromBag(ctx, *pts)
-					gp.hands[i].tiles = append(gp.hands[i].tiles, *pts)
-				}
-				continue
-			}
-			gp.hands = append(gp.hands, &HandState{
-				tiles: gp.bag[:len(p.Hand)],
-			})
-			gp.bag = gp.bag[len(p.Hand):]
-		}
-		log.Printf("initial bag: %v", gp.bag)
-		for i, hs := range gp.hands {
-			log.Printf("initial hand[%d]: %v", i, hs.tiles)
-		}
-	}
-
-	// Ensure any laid tiles are removed from our guessed bag, and
-	// if it's in a hand, swap it with one in the bag to keep the hand
-	// size correct (and guess at what they may have instead).
-	laidTiles := g.CurrentRound(ctx).LaidTiles
-	for _, lt := range laidTiles {
-		if gp.RemoveTileFromBag(ctx, *lt.Tile) {
-			continue
-		}
-		for i := range gp.hands {
-			if gp.RemoveTileFromHand(ctx, i, *lt.Tile) {
-				break
-			}
-		}
-	}
-
-	for i, p := range g.Players {
-		if i == gp.myPlayerIndex {
-			// for our own hand, check the guessed bag and other hands. If
-			// we remove it from one another hand, replace it with one from
-			// the bag.
-			for _, ht := range gp.hands[i].tiles {
-				if gp.RemoveTileFromBag(ctx, ht) {
-					continue
-				}
-				for oi := range gp.hands {
-					if oi == i {
-						continue
-					}
-					if gp.RemoveTileFromHand(ctx, oi, ht) {
-						gp.hands[oi].tiles = append(gp.hands[oi].tiles, gp.bag[0])
-						gp.bag = gp.bag[1:]
-						break
-					}
-				}
-			}
-			continue
-		}
-		extraTiles := len(p.Hand) - len(gp.hands[i].tiles)
-		if extraTiles > 0 {
-			// add tiles from the bag (they must have drawn)
-			gp.hands[i].tiles = append(gp.hands[i].tiles, gp.bag[:extraTiles]...)
-			gp.bag = gp.bag[extraTiles:]
-		}
-		if extraTiles < 0 {
-			// remove tiles from the hand (they must have laid)
-			removeCount := -extraTiles
-			removedTiles := gp.hands[i].tiles[:removeCount]
-			gp.bag = append(gp.bag, removedTiles...)
-			gp.hands[i].tiles = gp.hands[i].tiles[removeCount:]
-		}
+		gp.createInitialGuesses(ctx, g)
+	} else {
+		gp.fixBadGuesses(ctx, g)
 	}
 
 	//log.Printf("guessed bag: %v", gp.bag)
